a0aa36d8c5856a41b1831bd73a70414126b5ae43
codingbrains62 <codingbrains62@gmail.com>
2023-11-17 22:49:33 +0530

    route code change

--- routes/web.php
+++ routes/web.php
@@ -52,9 +52,25 @@ Route::middleware(['subuser'])->group(function () {
     Route::match(array('GET','POST'),'/showorganizationbyserch', [MessengerSubscriptionController::class,'showorganizationbyserch'])->name('showorganizationbyserch');
     Route::match(array('GET','POST'),'/addsubscription', [MessengerSubscriptionController::class,'addsubscription'])->name('addsubscription');
     Route::match(array('GET','POST'),'/updatenewssubs', [MessengerSubscriptionController::class,'updatenewssubs'])->name('updatenewssubs');
+<<<<<<< HEAD
      Route::get('/deletesubscription/{id}', [MessengerSubscriptionController::class,'deletesubscription']);
     Route::match(array('GET','POST'),'/changePasswrd', [MessengerSubscriptionController::class,'changePasswrd'])->name('changePasswrd');
     Route::get('/deletesubscriptionaccount/{id}', [MessengerSubscriptionController::class,'deletesubscriptionaccount']);
+=======
+<<<<<<< HEAD
+    Route::get('/deletesubscription/{id}', [MessengerSubscriptionController::class,'deletesubscription']);
+    Route::match(array('GET','POST'),'/changePasswrd', [MessengerSubscriptionController::class,'changePasswrd'])->name('changePasswrd');
+    Route::get('/deletesubscriptionaccount/{id}', [MessengerSubscriptionController::class,'deletesubscriptionaccount']);
+    Route::get('/sendtest/{id}', [MessengerSubscriptionController::class,'sendtest']);
+
+=======
+>>>>>>> 0dda513 (route code change)
+    
+
+    
+    
+
+>>>>>>> a5865c1 (route code change)
     
 =======
 >>>>>>> 20674ee (try to del unsub user)
